a934464d86d50380d3531b9a7962ad167301344a
= <=>
2022-08-03 13:07:40 +0530

    Merge sorted array and intersection of two arrays solution is added

---
+++ LeetCode/DataStructure/IntersectionOfTwoArray.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace LeetCode.DataStructure
+{
+    public class IntersectionOfTwoArray
+    {
+        public static int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Hashtable table = new Hashtable();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        if (table.ContainsKey(i) || table.ContainsValue(j))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            table.Add(i, j);
+                        }
+                    }
+                }
+            }
+            int[] result = new int[table.Keys.Count];
+            int[] temp = new int[table.Keys.Count];
+            table.Keys.CopyTo(temp, 0);
+            for (int m = 0; m < table.Keys.Count; m++)
+            {
+                result[m] = nums1[temp[m]];
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/DataStructure/MergeSortedArray.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.DataStructure
+{
+    public class MergeSortedArray
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var d = nums1.Length - n;
+            for (int i = nums1.Length - 1; i>=0 ; i--)
+            {
+                if (nums1[i] == 0)
+                {
+                    nums1[i] = (i-d < 0) ? nums1[i] : nums2[i-d];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < nums1.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums1.Length; j++)
+                {
+                    if (nums1[j] < nums1[i])
+                    {
+                        var temp = nums1[i];
+                        nums1[i] = nums1[j];
+                        nums1[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
